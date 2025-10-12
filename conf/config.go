a575edec4ee883e7c5beb1cc3cd7092ec21543bf
edisonch <edisonch@yahoo.com>
2025-10-13 04:25:20 +0700

    init commit

---
+++ conf/config.go
@@ -0,0 +1,231 @@
+package conf
+
+import (
+	_ "embed"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/knadh/koanf/providers/confmap"
+
+	"github.com/knadh/koanf/parsers/toml"
+	"github.com/knadh/koanf/providers/rawbytes"
+	k "github.com/knadh/koanf/v2"
+	"github.com/rs/zerolog"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var (
+	Koan    = k.New(".")
+	RunMode string
+	once    sync.Once
+	Log     *zerolog.Logger
+
+	//go:embed config.toml
+	confgo []byte
+
+	SAP_ASHOST string
+	SAP_USER   string
+	SAP_PWD    string
+	SAP_SYSNR  string
+	SAP_CLIENT string
+)
+
+const (
+	OS_WINDOWS      = 1
+	OS_MAC          = 2
+	OS_LINUX        = 3
+	OS_BSD          = 4
+	OS_UNIX_SOLARIS = 5
+	OS_UNIX_IBM     = 6
+	OS_MAINFRAME    = 7
+)
+
+func StartConfig() {
+	if err := Koan.Load(rawbytes.Provider(confgo), toml.Parser()); err != nil {
+		fmt.Printf("ERROR=%e\n", err)
+		return
+	}
+	RunMode = Koan.String("runmode")
+	Log = GetLogger()
+
+	GetSAP()
+}
+
+func GetLogger() *zerolog.Logger {
+	var zl *zerolog.Logger
+	once.Do(func() {
+		zl = configure()
+	})
+	return zl
+}
+
+func DetectOs() int {
+	os := runtime.GOOS
+	switch os {
+	case "windows":
+		return OS_WINDOWS
+	case "darwin":
+		return OS_MAC
+	case "linux": // linux family
+	case "android":
+		return OS_LINUX
+	case "netbsd": // BSD family
+	case "freebsd":
+	case "openbsd":
+	case "dragonfly":
+		return OS_BSD
+	case "solaris": // unix solaris
+	case "illumos": // unix solaris derivatives
+		return OS_UNIX_SOLARIS
+	case "aix": //unix ibm
+		return OS_UNIX_IBM
+	case "zos": //mainframe
+		return OS_MAINFRAME
+	}
+	return 0
+}
+
+func configure() *zerolog.Logger {
+	extractFilePath()
+	createFileFolder()
+	zerolog.TimeFieldFormat = time.RFC3339Nano
+	level := Koan.String(RunMode + ".logging_level")
+	switch level {
+	case "DEBUG":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "INFO":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "ERROR":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	}
+	var writers []io.Writer
+	if Koan.Bool(RunMode + ".console_logging") {
+		writers = append(writers, newConsoleWriter())
+	}
+	if Koan.Bool(RunMode + ".file_logging") {
+		writers = append(writers, newRollingFile())
+	}
+	mw := io.MultiWriter(writers...)
+
+	logger := zerolog.New(mw).With().Timestamp().Caller().Logger()
+
+	return &logger
+}
+
+func createFileFolder() {
+	if _, err := os.Stat(Koan.String("filepath")); os.IsNotExist(err) {
+		if err := os.MkdirAll(Koan.String("filepath"),
+			0777); err != nil {
+			panic(err)
+			return
+		}
+	}
+	if _, err := os.Stat(Koan.String("filestore")); os.IsNotExist(err) {
+		if err := os.MkdirAll(Koan.String("filestore"),
+			0774); err != nil {
+			panic(err)
+			return
+		}
+	}
+
+	if _, err := os.Stat(Koan.String("completeFilename")); err == nil {
+		fmt.Println("file exist")
+		return
+	}
+	_, err := os.OpenFile(Koan.String("completeFilename"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+	if err != nil {
+		panic(err)
+	}
+	//err = os.Chmod(Koan.String("completeFilename"), 0774)
+	//if err != nil {
+	//	panic(err)
+	//}
+}
+
+func newRollingFile() io.Writer {
+
+	lumberjackwriter := &lumberjack.Logger{
+		Filename:   Koan.String("completeFilename"),
+		MaxBackups: Koan.Int(RunMode + ".log_maxbackup"), // files
+		MaxSize:    Koan.Int(RunMode + ".log_maxsize"),   // megabytes
+		MaxAge:     Koan.Int(RunMode + ".log_maxage"),    // days
+		Compress:   true,
+	}
+	//fmt.Printf("rolling completeFilename %s\n", Koan.String("completeFilename"))
+	//writer := diode.NewWriter(lumberjackwriter, 1000, 10*time.Millisecond, func(missed int) {
+	//	fmt.Printf("Logger Dropped %d messages", missed)
+	//})
+	return lumberjackwriter
+}
+func newConsoleWriter() io.Writer {
+	output := zerolog.ConsoleWriter{Out: os.Stdout,
+		TimeFormat: time.RFC3339Nano, PartsOrder: []string{zerolog.
+				LevelFieldName, zerolog.TimestampFieldName,
+			zerolog.CallerFieldName, zerolog.MessageFieldName}}
+	output.FormatTimestamp = func(i interface{}) string {
+		return fmt.Sprintf(" %s | ", i)
+	}
+	output.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf(" %-6s|", i))
+	}
+	output.FormatMessage = func(i interface{}) string {
+		return fmt.Sprintf("---> %s", i)
+	}
+	output.FormatCaller = func(i interface{}) string {
+		return fmt.Sprintf(" %s ", i)
+	}
+	output.FormatFieldName = func(i interface{}) string {
+		return fmt.Sprintf(" %s: ", i)
+	}
+	output.FormatFieldValue = func(i interface{}) string {
+		return strings.ToLower(fmt.Sprintf(" %s ", i))
+	}
+	return output
+}
+
+func extractFilePath() {
+	Koan.Load(confmap.Provider(map[string]interface{}{"os": DetectOs()}, "."), nil)
+	Koan.Load(confmap.Provider(map[string]interface{}{"portapi": Koan.String(RunMode + ".port_api")}, "."), nil)
+	var filepath, filename, filestore string
+
+	if Koan.Int("os") == OS_WINDOWS {
+		filepath = Koan.String(RunMode + ".log_path_windows")
+		filename = Koan.String(RunMode + ".log_filename")
+		filestore = Koan.String(RunMode + ".file_path_windows")
+	} else {
+		filepath = Koan.String(RunMode + ".log_path")
+		filename = Koan.String(RunMode + ".log_filename")
+		filestore = Koan.String(RunMode + ".file_path")
+	}
+
+	completeFilename := filepath + string(os.PathSeparator) + filename
+
+	Koan.Load(confmap.Provider(map[string]interface{}{"completeFilename": strings.ToLower(completeFilename)}, "."), nil)
+	Koan.Load(confmap.Provider(map[string]interface{}{"filestore": filestore}, "."), nil)
+	Koan.Load(confmap.Provider(map[string]interface{}{"filepath": filepath}, "."), nil)
+}
+
+func CheckRunMode() {
+	StartConfig()
+	//createLogFile()
+	//file, err := os.OpenFile(Koan.String("completeFile"), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0744)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//defer file.Close()
+	Log.Info().Msgf("logging to %s with RunMode: %s", Koan.String("completeFilename"), RunMode)
+}
+
+func GetSAP() {
+	SAP_ASHOST = Koan.String(RunMode + ".sap_host")
+	SAP_USER = Koan.String(RunMode + ".sap_user")
+	SAP_PWD = Koan.String(RunMode + ".sap_pwd")
+	SAP_SYSNR = Koan.String(RunMode + ".sap_sysnr")
+	SAP_CLIENT = Koan.String(RunMode + ".sap_client")
+
+}
---
+++ db/migrate-sqlite/001_init_category.up.sql
@@ -0,0 +1,154 @@
+-- clothing_category contains all the available category for rental
+-- id contains the id for category
+-- clothes_cat_name contains the name of the category limit to 32 characters
+-- clothes_notes contains the notes for the category limit to 256 characters
+-- clothes_cat_status contains the status of the category: 1 = active, 2 = inactive
+-- created_at contains the date and time when the category is created
+-- updated_at contains the date and time when the category is updated
+create table clothing_category if not exists (
+    id integer primary key,
+    clothes_cat_name text not null,
+    clothes_notes text,
+    clothes_cat_status integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
+
+-- clothing_category_sub contains all the available subcategory for rental within the category
+-- id contains the id for subcategory
+-- id_clothing_category contains the id for the category
+-- clothes_cat_name_sub contains the name of the subcategory limit to 32 characters
+-- clothes_cat_location_sub contains the location of the subcategory limit to 64 characters
+-- clothes_picture_1 contains the picture of the subcategory using base64 encoding
+-- clothes_picture_2 contains the picture of the subcategory using base64 encoding
+-- clothes_picture_3 contains the picture of the subcategory using base64 encoding
+-- clothes_picture_4 contains the picture of the subcategory using base64 encoding
+-- clothes_picture_5 contains the picture of the subcategory using base64 encoding
+-- clothes_cat_status_sub contains the status of the subcategory: 1 = active, 2 = inactive
+-- created_at contains the date and time when the subcategory is created
+-- updated_at contains the date and time when the subcategory is updated
+create table clothing_category_sub if not exists (
+    id integer primary key,
+    id_clothing_category integer not null REFERENCES clothing_category(id),
+    clothes_cat_name_sub text not null,
+    clothes_cat_location_sub text not null,
+    clothes_picture_1 TEXT, -- base64 encoded image
+    clothes_picture_2 TEXT, -- base64 encoded image
+    clothes_picture_3 TEXT, -- base64 encoded image
+    clothes_picture_4 TEXT, -- base64 encoded image
+    clothes_picture_5 TEXT, -- base64 encoded image
+    clothes_cat_status_sub integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
+
+-- clothing_size contains all the available size for rental within the subcategory
+-- id contains the id for size
+-- id_clothing_category_sub contains the id for the category_sub
+-- clothes_size_name contains the name of the size limit to 8 characters
+-- clothes_size_notes contains the notes for the size limit to 256 characters
+-- clothes_size_status contains the status of the size: 1 = active, 2 = inactive
+-- created_at contains the date and time when the size is created
+-- updated_at contains the date and time when the size is updated
+create table clothing_size if not exists (
+    id integer primary key,
+    id_clothing_category_sub integer not null REFERENCES clothing_category_sub(id),
+    clothes_size_name text not null,
+    clothes_size_notes text,
+    clothes_size_status integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
+
+-- clothing_inventory_movement contains all the available inventory movement for rental within the subcategory
+-- id contains the id for inventory movement
+-- id_clothing_category_sub contains the id for the category_sub
+-- id_clothing_size contains the id for the size
+-- clothes_movement_action contains the action of the inventory movement: 1 = rent, 2 = return, 3 = add, 4 = remove
+-- clothes_qty_in contains the quantity of the inventory movement in
+-- clothes_qty_out contains the quantity of the inventory movement out
+-- clothes_qty_total contains the total quantity of the inventory movement
+-- clothes_cat_status_sub contains the status of the inventory movement: 1 = active, 2 = inactive
+-- created_at contains the date and time when the inventory movement is created
+-- updated_at contains the date and time when the inventory movement is updated
+create table clothing_inventory_movement if not exists (
+    id integer primary key,
+    id_clothing_category integer not null REFERENCES clothing_category_sub(id),
+    id_clothing_size integer not null REFERENCES clothing_size(id),
+    clothes_movement_action integer not null default 1,
+    clothes_qty_in integer not null,
+    clothes_qty_out integer not null,
+    clothes_qty_total integer not null,
+    clothes_cat_status_sub integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
+
+-- clothing_customer contains all the available customer for rental
+-- id contains the id for customer
+-- cust_name contains the name of the customer limit to 64 characters
+-- cust_address contains the address of the customer limit to 256 characters
+-- cust_city contains the city of the customer limit to 64 characters
+-- cust_phone contains the phone of the customer limit to 16 characters
+-- cust_email contains the email of the customer limit to 128 characters
+-- cust_notes contains the notes for the customer limit to 256 characters
+-- cust_status contains the status of the customer: 1 = active, 2 = inactive
+-- created_at contains the date and time when the customer is created
+create table clothing_customer if not exists (
+    id integer primary key,
+    cust_name text not null,
+    cust_address text not null,
+    cust_city text not null,
+    cust_phone text not null,
+    cust_email text not null,
+    cust_notes text,
+    cust_status integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
+
+-- clothing_rental contains all the available rental for customer
+-- id contains the id for rental
+-- id_clothing_category_sub contains the id for the category_sub
+-- id_clothing_size contains the id for the size
+-- id_clothing_customer contains the id for the customer
+-- clothes_qty_rent contains the quantity of the rental
+-- clothes_qty_return contains the quantity of the return
+-- clothes_cat_status_sub contains the status of the rental: 1 = active, 2 = cancel, 3 = return, 4 = loss
+-- clothes_rent_date_begin contains the date and time when the rental is begin
+-- clothes_rent_date_end contains the date and time when the rental is end
+-- clothes_rent_date_actual_pickup contains the date and time when the pickup is actual pickup
+-- clothes_rent_date_actual_return contains the date and time when the rental is actual return
+-- created_at contains the date and time when the rental is created
+-- updated_at contains the date and time when the rental is updated
+create table clothing_rental if not exists (
+    id integer primary key,
+    id_clothing_category_sub integer not null REFERENCES clothing_category_sub(id),
+    id_clothing_size integer not null REFERENCES clothing_size(id),
+    id_clothing_customer integer not null REFERENCES clothing_customer(id),
+    clothes_qty_rent integer not null default 1,
+    clothes_qty_return integer not null default 0,
+    clothes_rent_date_begin datetime not null,
+    clothes_rent_date_end datetime not null,
+    clothes_rent_date_actual_pickup datetime not null,
+    clothes_rent_date_actual_return datetime not null,
+    clothes_cat_status_sub integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
+
+-- clothing_users contains all the available users for rental
+-- id contains the id for users
+-- username contains the username of the users limit to 32 characters
+-- pin contains the pin of the users limit to 6 digits
+-- user_status contains the status of the users: 1 = active, 2 = inactive
+-- created_at contains the date and time when the users is created
+-- updated_at contains the date and time when the users is updated
+create table clothing_users if not exists (
+    id integer primary key,
+    username text not null,
+    pin integer not null default 123456,
+    user_status integer not null default 1,
+    created_at datetime not null,
+    updated_at datetime not null
+);
